447110137cebed6b0ef2dcc681bcc1396fb22c3a
cfcsaditya <lohani.aditya@cfcsindia.com>
2026-03-31 15:55:09 +0530

    donesecondtask

--- App.tsx
+++ App.tsx
@@ -14,21 +14,7 @@ import Dashboard from './Dashboard';
 LogBox.ignoreAllLogs();
 const App = () => {
   return (
-    <SafeAreaView style={{flex:1}} edges={['top']}>
-      <StatusBar
-        barStyle="dark-content"
-     
-        
-      />
-      
-      <View style={{ ...styles.box, backgroundColor: Colors.orange }} />
-      <View style={[styles.box,styles.middleContainer]}>
-        <View style={styles.circle} />
-      </View>
-      <View style={{...styles.box,backgroundColor:Colors.green}} />
-    
-        
-    </SafeAreaView>
+    <Dashboard/>
      
   );
 };
@@ -36,26 +22,5 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({
-  box: {
-    flex: 1,
-  },
-    
-  middleContainer: {
 
-    backgroundColor:Colors.white,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  circle: {
-    borderWidth: vw(7),
-    borderRadius: vw(135),
-    borderColor: Colors.blue,
 
-    height: vh(200),
-    width: vw(200),
-
-    backgroundColor: 'white',
-  },
-   
-});
